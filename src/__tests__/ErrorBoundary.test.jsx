6d4168388d8a25dc89c7802f8da268a9e674f7dd
Rodrigo <rodrigo_ferreira20.04@hotmail.com>
2025-09-17 19:49:33 -0300

    Update ErrorBoundary.test.jsx

--- src/__tests__/ErrorBoundary.test.jsx
+++ src/__tests__/ErrorBoundary.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import ErrorBoundary from '../components/ErrorBoundary'; // se tiver
+import ErrorBoundary from '../components/ErrorBoundary'; 
 
 function Bomb() {
   throw new Error('Falha forçada');
